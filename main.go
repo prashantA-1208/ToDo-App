0cac01b94da5bd8e7fbb20c1a1465b00811f980f
Prashant <aryanprashant123@gmail.com>
2025-04-13 18:04:16 +0530

    Added handlers

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"prashantA-1208/ToDo-App.git/handlers"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main(){
-	fmt.Println("Hello World")
+func main() {
+	router := gin.Default()
+
+	router.GET("/tasks", handlers.GetTasks)
+	router.GET("/tasks/:id", handlers.GetTaskByID)
+	router.POST("/tasks", handlers.CreateTask)
+	router.PUT("/tasks/:id", handlers.UpdateTask)
+	router.DELETE("/tasks/:id", handlers.DeleteTask)
+
+	router.Run(":8080")
+
 }
